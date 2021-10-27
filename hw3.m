548d6af404b835b3a0deef5d3654f224c227dd74
jazelsug <70077249+jazelsug@users.noreply.github.com>
2021-10-27 14:43:51 -0700

    greedy action selection and take action setup

--- hw3.m
+++ hw3.m
@@ -1,6 +1,6 @@
 % Name: hw3.m
 % Author: Jazel A. Suguitan
-% Last Modified: Oct. 26, 2021
+% Last Modified: Oct. 27, 2021
 
 clc,clear
 close all
@@ -21,21 +21,36 @@ A = 1:num_actions; % 1-up, 2-down, 3-right, 4-left
 num_eps = 6;
 num_its = 100;
 delta_t_update = 0.008;
-T = 0:delta_t_update:7; % Set simulation time
-Q = cell(num_states, num_actions);   % create initial Q-table  zeros(num_states, 4)
-s_ = cell(size(T), num_its);
-a_ = cell(size(T,1), num_its);
-
-%Q (s_{0}, a_{0})(0) = zeros(num_states, num_actions);
+%T = 0:delta_t_update:7; % Set simulation time
+%Q = cell(num_states, num_actions);   % create initial Q-table  zeros(num_states, 4)
+Q = zeros(num_states, num_actions);
+%s_ = cell(size(T), num_its);
+%a_ = cell(size(T,1), num_its);
+Q_eps = cell(num_eps, 1);
 
 for episode = 1:num_eps
-    t = episode;    %extra variable t to make Q equation look cleaner
-    s_{t} = 1;  %place robot in upper left cell of grid
+    %t = episode;    %extra variable t to make Q equation look cleaner
+    %s_{t} = 1;  %place robot in upper left cell of grid
+    s_t = 1;    %place robot in upper left cell of grid
     for iteration = 1:num_its
-        k = iteration;  % extra variable k to make Q equation look cleaner
-        if iteration == 1 %first iteration, Q table is empty
-           
+        %k = iteration;  % extra variable k to make Q equation look cleaner
+        %find max reward in row of Q-table corresponding with current state
+        [maxReward, a_next] = max(Q(s_t));
+        
+        %take action and observe
+        
+        if a_next == 1
+            %observe
+        elseif a_next == 2
+            %observe
+        elseif a_next == 3
+            %
+        else
+            %
         end
-        %Q (s_{t}, a_{t})(k) = Q(s_{t}, a_{t})(k-1) + alpha [ r_{t+1} + gamma * Q(s_{t+1}, a_{t+1})(k-1)- Q(s_{t}, a_{t})(k-1)]
+        
+        %Q = Q + alpha * ();
+        %FROM SLIDES: Q (s_{t}, a_{t})(k) = Q(s_{t}, a_{t})(k-1) + alpha [ r_{t+1} + gamma * Q(s_{t+1}, a_{t+1})(k-1)- Q(s_{t}, a_{t})(k-1)]
     end
+    Q_eps{episode} = Q;
 end
--- test.m
+++ test.m
@@ -1,18 +1,36 @@
-%something in the background
-string = mat2cell(num2str([1:10*10]'),ones(10*10,1));
-%display the background
-imagesc(I)
-hold on
-%insert the labels
-I = magic(10)
-%generate where each text will go
-[X Y]=meshgrid(1:10,1:10);
-%create the list of text
-text(Y(:)-.5,X(:)+.25,string,'HorizontalAlignment','left')
-%calculte the grid lines
-grid = .5:1:10.5;
-grid1 = [grid;grid];
-grid2 = repmat([.5;10.5],1,length(grid))
-%plot the grid lines
-plot(grid1,grid2,'k')
-plot(grid2,grid1,'k')
+% %something in the background
+% string = mat2cell(num2str([1:10*10]'),ones(10*10,1));
+% %display the background
+% imagesc(I)
+% hold on
+% %insert the labels
+% I = magic(10)
+% %generate where each text will go
+% [X Y]=meshgrid(1:10,1:10);
+% %create the list of text
+% text(Y(:)-.5,X(:)+.25,string,'HorizontalAlignment','left')
+% %calculte the grid lines
+% grid = .5:1:10.5;
+% grid1 = [grid;grid];
+% grid2 = repmat([.5;10.5],1,length(grid))
+% %plot the grid lines
+% plot(grid1,grid2,'k')
+% plot(grid2,grid1,'k')
+
+%Generate the X and Y grid arrays using the MESHGRID function.
+x = [1:6];
+y = [1:4];
+[X,Y] = meshgrid(x,y)
+%Note that size(Z) is the same as size(x) and size(y)
+Z = [1 0 -2 1 2 1;2 -2 0 -1 0 1;1 0 0 -2 2 1;1 1 1 1 1 1];
+% % create a colormap having RGB values of dark green,
+% %light green, white, dark red and light red.
+% map2 = [0 1 0; 0 0.8 0;1 1 1;0.6 0 0;1 0 0 ];
+% %use the user defined colormap for figure.
+% colormap(map2);
+%plot the figure
+pcolor(X,Y,Z);
+% %set the x and y labels
+% set(gca,'XTick',[1 2 3 4 5 6],'YTick',[1 2 3 4],'XTicklabel',[' ';'a';'b'; 'c'; 'd';'e'],'YTicklabel',[' ';'f';'g';'h']);
+% %set the color limits
+% caxis([-2 2])
